e54ac52958664e1d5f25046bba6a50bfaef0a02e
Diganta Singh Panwar <114343344+digantaa@users.noreply.github.com>
2025-09-28 15:28:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(() =>{
+    const saved = localStorage.getItem('count')
+    const parsedCount =  saved ? Number(saved):0
+    if (!saved || isNaN(parsedCount)) {
+    return 0;
+  }
+
+  return parsedCount >= 100 ? 0 : parsedCount;
+  })
+
+
+  useEffect(() =>{
+    localStorage.setItem('count',count)
+  },[count])
+
+  
+
+  return (
+    <>
+    
+      <div className="card flex flex-col gap-4 justify-center items-center min-h-screen">
+        <button  className=' rounded-2xl px-5 py-3 border bg-rose-400 hover:bg-rose-700 text-white' onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      <p className="text-pink-400 text-3xl">
+        Click on the Vite and React logos to learn more
+      </p>
+      </div>
+    </>
+  )
+}
+
+export default App
